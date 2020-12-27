16770d4c701257b45afa07bc27ed1a5dfc768280
Xriuk <hudylkor@gmail.com>
2020-12-27 20:25:12 +0100

    Added Find method

--- Assets/StoneShelter/GUID.cs
+++ Assets/StoneShelter/GUID.cs
@@ -9,14 +9,46 @@ namespace StoneShelter{
     [AddComponentMenu("Stone Shelter/Core/GUID")]
     public class GUID : MonoBehaviour{
         // Static data
+        /// <summary>
+        /// Value to set in prefabs to auto generate the GUID in instances
+        /// </summary>
         public const string autogenerateValue = "AUTO";
+        protected static Dictionary<string, GUID> m_data = new Dictionary<string, GUID>();  // Active instances
+
+        [RuntimeInitializeOnLoadMethod]
+        protected static void RegisterInactiveObjects(){
+            foreach(GUID guid in Resources.FindObjectsOfTypeAll<GUID>()) {
+                m_data[guid.m_guid] = guid;
+            }
+        }
+
+        /// <summary>
+        /// Finds an object by its GUID
+        /// </summary>
+        /// <param name="guid">unique identifier</param>
+        /// <returns>GUID if found, null otherwise</returns>
+        public static GUID Find(string guid){
+#if UNITY_EDITOR
+			if(!EditorApplication.isPlaying){
+                foreach(GUID gd in Resources.FindObjectsOfTypeAll<GUID>()) {
+                    if(gd.m_guid == guid)
+                        return gd;
+                }
+                return null;
+            }
+#endif
+
+            if(m_data.ContainsKey(guid))
+                return m_data[guid];
+            return null;
+		}
 
 
         // Configurable variables
         [Tooltip("Unique ID of the component, leave it empty to generate a new one")]
         [SerializeField]
         protected string m_guid = "";
-        [Tooltip("If true the GUID can be generated and reset by the Prefab Asset")]
+        [Tooltip("If true the GUID will be inherited from the Prefab Asset")]
         [SerializeField]
         protected bool m_guidPrefab = true;
 
@@ -28,18 +60,25 @@ namespace StoneShelter{
 
 
 		protected void Reset(){
-			#region prefab
+            #region prefab
 #if UNITY_EDITOR
 			if(PrefabStageUtility.GetCurrentPrefabStage() != null || PrefabUtility.IsPartOfPrefabAsset(this)){
                 m_guid = autogenerateValue;
                 return;
             }
 #endif
-			#endregion
+            #endregion
 
             // Generate a new GUID
 			GenerateGUID();
         }
+		protected void Awake(){
+            m_data[m_guid] = this;
+		}
+		protected void OnDestroy(){
+            if(m_data.ContainsKey(m_guid))
+                m_data.Remove(m_guid);
+		}
 
 		/// <summary>
 		/// Generate a new GUID
@@ -48,7 +87,7 @@ namespace StoneShelter{
             // Generate a new GUID
             m_guid = Guid.NewGuid().ToString();
 
-			#region prefab + editor
+#region prefab + editor
 #if UNITY_EDITOR
 			// If this is part of a prefab we want to override the property of the instance to save the GUID correctly
 			if(PrefabUtility.IsPartOfNonAssetPrefabInstance(this))
@@ -59,7 +98,7 @@ namespace StoneShelter{
 
             Debug.Log("<color=yellow>[" + gameObject.name + "(" + this.GetType().Name + ")]</color> Generated new GUID", gameObject);
 #endif
-			#endregion
+#endregion
 		}
 
         /// <summary>
@@ -69,7 +108,7 @@ namespace StoneShelter{
             // Reset the GUID
             m_guid = "";
 
-			#region prefab + editor
+            #region prefab + editor
 #if UNITY_EDITOR
 			// If this is part of a prefab we want to override the property of the instance to save the GUID correctly
 			if(PrefabUtility.IsPartOfNonAssetPrefabInstance(this))
@@ -80,11 +119,11 @@ namespace StoneShelter{
 
             Debug.Log("<color=yellow>[" + gameObject.name + "(" + this.GetType().Name + ")]</color> GUID reset", gameObject);
 #endif
-			#endregion
+            #endregion
 		}
 
 
-		#region context menu + editor
+        #region context menu + editor
 #if UNITY_EDITOR
 		[MenuItem("CONTEXT/GUID/Generate GUID")]
         protected static void GenerateGUIDStatic(MenuCommand command){
@@ -111,8 +150,7 @@ namespace StoneShelter{
             EditorUtility.SetDirty(this);
 
             Debug.unityLogger.logEnabled = false;
-            GUID[] objs = (GUID[])Resources.FindObjectsOfTypeAll(typeof(GUID));
-            foreach(GUID guid in objs) {
+            foreach(GUID guid in Resources.FindObjectsOfTypeAll<GUID>()) {
                 // Check if it is part of a prefab instance and if it doesn't already have a guid
                 if(!guid.m_guidPrefab || !PrefabUtility.IsPartOfNonAssetPrefabInstance(guid) || PrefabUtility.GetPrefabAssetPathOfNearestInstanceRoot(guid) != assetPath)
                     continue;
@@ -132,8 +170,7 @@ namespace StoneShelter{
             if(PrefabStageUtility.GetCurrentPrefabStage() != null)
                 return;
 
-            GUID[] objs = (GUID[])Resources.FindObjectsOfTypeAll(typeof(GUID));
-            foreach(GUID gd in objs) {
+            foreach(GUID gd in Resources.FindObjectsOfTypeAll<GUID>()) {
                 // Check if it is part of a prefab instance and if it doesn't already have a guid
                 if(!gd.m_guidPrefab || gd.m_guid != autogenerateValue || !PrefabUtility.IsPartOfNonAssetPrefabInstance(gd))
                     continue;
