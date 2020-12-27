ce7a29b9301c4e131acfe5afe26316767f13b7fc
Xriuk <hudylkor@gmail.com>
2020-12-27 20:33:31 +0100

    Editor edits

--- Assets/StoneShelter/Editor/GUIDEditor.cs
+++ Assets/StoneShelter/Editor/GUIDEditor.cs
@@ -7,23 +7,19 @@ using UnityEngine;
 namespace StoneShelter{
     [CustomEditor(typeof(GUID), true)]
     public class GUIDEditor : Editor{
-        override public void OnInspectorGUI(){
-            // Draw script
-            using(new EditorGUI.DisabledScope(true)){
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("m_Script"));
-            }
+        protected const string lastGUIDProp = "m_guidPrefab";
 
-            // Draw GUID Field
+        protected void DrawGUID() {
             EditorGUILayout.BeginHorizontal();
             string guid = serializedObject.FindProperty("m_guid").stringValue;
             EditorGUILayout.PropertyField(serializedObject.FindProperty("m_guid"));
-            if(PrefabUtility.IsPartOfNonAssetPrefabInstance(serializedObject.targetObject)){
+            if(PrefabUtility.IsPartOfNonAssetPrefabInstance(serializedObject.targetObject)) {
                 bool prev = serializedObject.FindProperty("m_guidPrefab").boolValue;
                 GUIStyle style = EditorStyles.label;
                 EditorGUILayout.PropertyField(serializedObject.FindProperty("m_guidPrefab"), new GUIContent("", serializedObject.FindProperty("m_guidPrefab").tooltip), GUILayout.Width(style.fontSize + EditorGUIUtility.standardVerticalSpacing));
                 bool curr = serializedObject.FindProperty("m_guidPrefab").boolValue;
-                if(curr != prev){
-                    if(!prev && curr){
+                if(curr != prev) {
+                    if(!prev && curr) {
                         Debug.unityLogger.logEnabled = false;
                         ((GUID)serializedObject.targetObject).ResetGUID();
                         Debug.unityLogger.logEnabled = true;
@@ -31,7 +27,7 @@ namespace StoneShelter{
 
                         Debug.Log("<color=yellow>[" + ((GUID)serializedObject.targetObject).gameObject.name + "(" + this.GetType().Name + ")]</color> Linked GUID to prefab", ((GUID)serializedObject.targetObject).gameObject);
                     }
-					else{
+                    else {
                         serializedObject.FindProperty("m_guid").stringValue = guid + "$$$";
                         serializedObject.ApplyModifiedProperties();
                         serializedObject.FindProperty("m_guid").stringValue = guid;
@@ -40,8 +36,8 @@ namespace StoneShelter{
                     }
                 }
             }
-            else if(PrefabStageUtility.GetCurrentPrefabStage() != null || PrefabUtility.IsPartOfPrefabAsset(serializedObject.targetObject)){
-                if(GUILayout.Button(GUID.autogenerateValue, GUILayout.Width(50))){
+            else if(PrefabStageUtility.GetCurrentPrefabStage() != null || PrefabUtility.IsPartOfPrefabAsset(serializedObject.targetObject)) {
+                if(GUILayout.Button(GUID.autogenerateValue, GUILayout.Width(50))) {
                     // Set the GUID for the prefab
                     serializedObject.FindProperty("m_guid").stringValue = GUID.autogenerateValue;
 
@@ -49,14 +45,24 @@ namespace StoneShelter{
                 }
 
                 // If we have changed the GUID we update all the linked instances
-                if(guid != serializedObject.FindProperty("m_guid").stringValue){
+                if(guid != serializedObject.FindProperty("m_guid").stringValue) {
                     ((GUID)serializedObject.targetObject).PrefabUpdateLinkedInstancesGUID();
                 }
             }
             EditorGUILayout.EndHorizontal();
+        }
+
+        override public void OnInspectorGUI(){
+            // Draw script
+            using(new EditorGUI.DisabledScope(true)){
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("m_Script"));
+            }
+
+            // Draw GUID Field
+            DrawGUID();
 
             // Draw default properties
-            SerializedProperty prop = serializedObject.FindProperty("m_guidPrefab");
+            SerializedProperty prop = serializedObject.FindProperty(lastGUIDProp);
 			while(prop.NextVisible(false)){
                 EditorGUILayout.PropertyField(prop);
 			}
